11631b6f86fad82b4bca0d01b93e6cc8ca30b531
Br0ck25 <jamesbrock25@hotmail.com>
2025-05-21 17:05:04 -0400

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,16 @@
+// public/service-worker.js
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("gps-cache").then((cache) =>
+      cache.addAll(["/", "/index.html", "/main.js"])
+    )
+  );
+});
 
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request)
+    )
+  );
+});
