47fb79d6eb602cbe6bd7a512f1c95ad8a88cad4d
James <jamesbrock25@hotmail.com>
2025-05-28 17:13:37 -0400

    PWA

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -3,7 +3,7 @@ import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { feature } from "topojson-client";
 
-function isDuplicateLine(lineFeature, mainLines, threshold = 0.05) {
+function isDuplicateLine(lineFeature, mainLines, threshold = 0.01) {
   const coords = lineFeature.geometry?.coordinates;
   if (!coords || !Array.isArray(coords)) return false;
 
